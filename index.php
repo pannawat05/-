5d6a4ba57987c44ac86929b0ac6752882448ae22
pannawat05 <83331747+pannawat05@users.noreply.github.com>
2022-11-23 19:49:53 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -5,11 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Basic PHP</title>
 </head>
 <body>
 <div class="container">
-    <nav>Header bar</nav>
+    <nav>
+    <a href="#">Home</a>
+    <a href="#">About</a>
+    <a href="#">Product</a>
+    <a href="#">Contact</a>
+    <div class="dropdown">
+        <button class="dropbtn">
+        <i class="fa-solid fa-square-caret-down"></i>Dropdown
+        </button>
+    <div class="dropdown-contentent">
+        <a href="#">link1</a>
+        <a href="#">link2</a>
+        <a href="#">link3</a>
+    </div>
+    </div>
+    </nav>
     <div class="sidebar">sidebar</div>
     <div class="catalog">catalog</div>
     <footer>Created by...</footer>
