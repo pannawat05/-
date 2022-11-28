cf42ed173638f5042405ad7b1530a7ffd1b9cd97
pannawat05 <83331747+pannawat05@users.noreply.github.com>
2022-11-28 20:00:01 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -6,16 +6,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <title>Basic PHP</title>
 </head>
 <body>
 <div class="container">
     <nav>
-    <a href="#">Home</a>
-    <a href="#">About</a>
-    <a href="#">Product</a>
-    <a href="#">Contact</a>
-    <div class="dropdown">
+      <!-- toggle icon-->
+    <div class="navcontent">
+        <!-- logo-->
+<a id="logo" href="#!">My Catalog!</a>
+<!--menu-->
+    <a href="#" id="link">Home</a>
+    <a href="#" id="link">About</a>
+    <a href="#" id="link">Product</a>
+    <a href="#" id="link">Contact</a>
+    <div class="dropdown" id="link">
         <a class="dropbtn">
         <i class="fa-solid fa-square-caret-down"></i>Dropdown
         </a>
@@ -25,7 +32,7 @@
   <a href="#">link3</a>
     </div>
 </div>
-    
+    </div>
     </nav>
     <div class="sidebar">sidebar</div>
     <div class="catalog">catalog</div>
