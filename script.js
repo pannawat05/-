4d43b71324f70acf3eba120bd1159a998c667066
pannawat05 <83331747+pannawat05@users.noreply.github.com>
2022-12-01 12:39:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function toggle(){
 	const toggle = document.getElementByClassName('.container')
 
 	if(toggle.className == "container"){
-		toggle.classlist.add('mobile')
+		toggle.classList.add('mobile')
 	}else{
-		toggle.classlist.remove('mobile')
+		toggle.classList.remove('mobile')
 	}
 
 
