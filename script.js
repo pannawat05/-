3ac420e1d76498c2a5d2727f370005246534ec44
pannawat05 <83331747+pannawat05@users.noreply.github.com>
2022-12-01 09:43:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,11 @@
 function toggle(){
 	const toggle = document.getElementByClassName('container')
 
-	loggle.style.grid-template-rows="3fr 6fr 1.2fr 0.6fr";
-	toggle.style.grid-template-columns"1fr"
-	toggle.style.grid-template-areas"'nav''cat''side''footer'"
+	if(toggle.className == "container"){
+		toggle.className +="-mobile"
+	}else{
+		toggle.className = "container"
+	}
 
 
 }
